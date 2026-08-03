62ef1ba51fd3ebd15c9cc48034424ee279d3bb04
luisjuarez-dev <luis.beto642@gmail.com>
2026-08-03 14:27:31 -0600

    Se renombró los nombres de las funciones para rutas

--- PruebaStartEvent/CodigoFuente/Flask/aplicacion/rutas/autenticacion.py
+++ PruebaStartEvent/CodigoFuente/Flask/aplicacion/rutas/autenticacion.py
@@ -6,20 +6,15 @@ from aplicacion.controlador.autenticacion import AutenticacionControlador
 controlador = AutenticacionControlador(AutenticadorServicio())
 
 @app.route("/login", methods=["POST"])
-def login():
+def iniciar_sesion():
     correo = request.form["correo"]
     password = request.form["password"]
     return controlador.iniciar_sesion(correo, password)
     
 @app.route('/')
-def home():
+def login():
     return controlador.renderizar_login()
 
 @app.after_request
 def refresh(response):
     return controlador.refrescar_tokens_por_expirar(response)
-
-#@app.before_request
-#def refrescar_expirado(response):
-#    return controlador.refrescar_tokens_expirados()
-    
