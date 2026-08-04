a63a294e566d077b566d60feead3a4ba4d3f5635
luisjuarez-dev <luis.beto642@gmail.com>
2026-08-03 18:08:39 -0600

    Tiempo de expiracion de jwt a 2 horas

--- PruebaStartEvent/CodigoFuente/Flask/aplicacion/inicializacion/config.py
+++ PruebaStartEvent/CodigoFuente/Flask/aplicacion/inicializacion/config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://admin_agent:9q70MdN915@localhost:5432/isolated_db_test'
     JWT_SECRET_KEY = '123'
     JWT_TOKEN_LOCATION = ["cookies"]
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=1)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=2)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     DIRECTORIO_DOCUMENTOS = DIRECTORIO_STARTEVENT / 'Documentos'
     DIRECTORIO_BASE_FLASK = ARCHIVO_EJECUTANDODSE.parents[2]  # "/Flask"
