601ec2d9220442ef47cd9ec8e7c5fa1b86e5df69
luisjuarez-dev <luis.beto642@gmail.com>
2026-09-21 22:08:40 -0600

    Avances de componentes

--- src/controlador/componentes.py
+++ src/controlador/componentes.py
@@ -6,13 +6,13 @@ from flask import (
     render_template_string,
     request,
 )
-from jinja2 import Template
 
 
 class ComponentesControlador:
     """Controlador para el blueprint de componentes.
 
-    Atiende peticiones relacionadas al funcionamiento dinámico de los componentes
+    Atiende peticiones relacionadas al funcionamiento dinámico de los
+    componentes
     """
 
     def renderizar_playground(self) -> str:
@@ -30,8 +30,9 @@ class ComponentesControlador:
         if not nombre_input:
             raise Exception  # TODO(luis): Crear una excepción o respuesta para
             # no actualizar la vista htmx
-        valor = request.args.get(nombre_input)
         input_color_macro = get_template_attribute(
-            "componentes/form_inputs.html", "input_color"
-        )
-        return input_color_macro(value=valor)
+                    "componentes/form_inputs.html", "input_color"
+                )
+        valor = request.args.get(nombre_input)
+        label = request.args.get("label")
+        return input_color_macro(label=label, name=nombre_input, value=valor)
